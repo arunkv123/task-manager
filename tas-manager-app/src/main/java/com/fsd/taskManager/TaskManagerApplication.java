43a585e180205329d1c31f6cbaa4471a9b16d185
Admin <arunkv.vit@gmail.com>
2018-06-26 04:50:30 +0530

    initial commit

---
+++ tas-manager-app/src/main/java/com/fsd/taskManager/TaskManagerApplication.java
@@ -0,0 +1,15 @@
+package com.fsd.taskManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan(basePackages = { "com.fsd.taskManager" })
+public class TaskManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+}

05cfb283ad0101cfa52247d56790ff57e4aca9d1
Admin <arunkv.vit@gmail.com>
2018-08-20 00:26:28 +0530

    performance test added

--- task-manager-app/src/main/java/com/fsd/taskManager/TaskManagerApplication.java
+++ task-manager-app/src/main/java/com/fsd/taskManager/TaskManagerApplication.java
@@ -2,20 +2,30 @@ package com.fsd.taskManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @EnableAutoConfiguration
 @Configuration
-//@EnableOAuth2Sso
+@EnableAsync
+// @EnableOAuth2Sso
 @ComponentScan(basePackages = { "com.fsd.taskManager" })
-public class TaskManagerApplication {
+public class TaskManagerApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TaskManagerApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
-	
+
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
--- task-manager-app/src/test/java/com/taskManager/test/TaskManagerTest.java
+++ task-manager-app/src/test/java/com/taskManager/test/TaskManagerTest.java
@@ -6,7 +6,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.Future;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +21,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.fsd.taskManager.TaskManagerApplication;
 import com.fsd.taskManager.service.TaskService;
+import com.fsd.taskManager.vo.SearchParam;
 import com.fsd.taskManager.vo.TaskDetails;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebMvcTest
+//@WebMvcTest
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = TaskManagerApplication.class)
 public class TaskManagerTest {
 
 	@Autowired
+	private WebApplicationContext context;
+	
 	private MockMvc mockMvc;
 	
 	@LocalServerPort
@@ -39,6 +52,11 @@ public class TaskManagerTest {
 
 	@Autowired
 	private TaskService taskService;
+	
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
 	@Test
 	public void addTaskTest() {
 		TaskDetails request = new TaskDetails();
@@ -51,6 +69,47 @@ public class TaskManagerTest {
 		assertEquals("Success", response.getBody());
 	}
 	
+	/**
+	 * 
+	 */
+	@Test
+	public void performanceTest() {
+		SearchParam request = new SearchParam();
+		request.setPriorityFrom(0);
+		request.setPriorityTo(30);
+		ResponseEntity<List> initalList = restTemplate.postForEntity("http://localhost:"+serverPort+"/viewTask", request, List.class);
+		int initialRows =initalList.getBody().size();
+		try {
+			Map<Integer,Future<ResponseEntity<String>>> result = new HashMap<>(); 
+			for(int i=0;i<1000;i++) {
+				System.out.println("thread "+i+" started");
+				result.put(i, post());
+			}
+			for (Integer pos : result.keySet()) {
+				System.out.println("the result for data in position "+pos+" is "+result.get(pos).get().getBody());
+			}
+			ResponseEntity<List> newList = restTemplate.postForEntity("http://localhost:"+serverPort+"/viewTask", request, List.class);
+			int newRows =newList.getBody().size();
+			assertEquals(998 , newRows-initialRows);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Async
+	private Future<ResponseEntity<String>> post(){
+		Random rand = new Random();
+		TaskDetails request = new TaskDetails();
+		request.setParentTask("Sprint2");
+		request.setTask("Test task"+rand.nextInt());
+		request.setPriority(12);
+		request.setStartDate(new Date());
+		request.setEndDate(new Date());
+		ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:"+serverPort+"/addTask", request, String.class);
+		//assertEquals("Success", response.getBody());
+		return new AsyncResult<>(response);
+	}
 	@Test
 	public void addTaskMockTest() throws Exception {
 		TaskDetails request = new TaskDetails();
@@ -61,6 +120,15 @@ public class TaskManagerTest {
 		request.setEndDate(new Date());
 		when(taskService.addTask(request)).thenReturn(true);
 		this.mockMvc.perform(get("/addTask")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void viewTaskMockTest() throws Exception {
+		SearchParam request = new SearchParam();
+		request.setPriorityFrom(0);
+		request.setPriorityTo(30);
+		ResponseEntity<List> response = restTemplate.postForEntity("http://localhost:"+serverPort+"/viewTask", request, List.class);
+		System.out.println(response.getBody().size());
 	}
 	
 }
